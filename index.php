2720da065876fa2c0e816051ee4eb43da9b92d61
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2025-08-04 19:04:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,18 +7,24 @@ $currentDomain = $_SERVER['HTTP_HOST'];
 
 $listDomain = 'domains.ggio.fr';
 
-?><?php echo count($domains); ?> noms de domaine réservés par <a href="https://miniggiodev.fr" target="blank">Pierre</a> :
-<ul>
-    <?php foreach ($domains as $domain): ?>
-        <li><a
-            href="https://<?php echo $domain ?>"
-            <?php if (strpos($_SERVER['HTTP_HOST'], $domain) !== false): ?>
-                style="font-weight: bold;"
-            <?php endif; ?>
-        ><?php echo $domain ?></a></li>
-    <?php endforeach; ?>
-</ul>
-
-<?php if ($currentDomain !== $listDomain) {
-    echo '<br>Liste aussi disponible sur <a href="https://' . $listDomain . '">' . $listDomain . '</a>';
-} ?>
+?>
+<head>
+    <title><?php echo count($domains); ?> noms de domaine réservés par Pierre</title>
+</head>
+<body>
+    <h1 style="font-size: 1.3rem"><?php echo count($domains); ?> noms de domaine réservés par <a href="https://miniggiodev.fr" target="blank">Pierre</a> :</h1>
+    <ul>
+        <?php foreach ($domains as $domain): ?>
+            <li><a
+                href="https://<?php echo $domain ?>"
+                <?php if (strpos($_SERVER['HTTP_HOST'], $domain) !== false): ?>
+                    style="font-weight: bold;"
+                <?php endif; ?>
+            ><?php echo $domain ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+    
+    <?php if ($currentDomain !== $listDomain) {
+        echo '<br>Liste aussi disponible sur <a href="https://' . $listDomain . '">' . $listDomain . '</a>';
+    } ?>
+</body>
